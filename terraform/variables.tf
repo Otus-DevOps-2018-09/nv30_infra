b90c19a7c83774bcdc3c60d7a70e7c90028b64e2
Nikita Gorlov <nv30over@gmail.com>
2018-11-19 10:34:25 -0500

    Removed key variables for additional users

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,14 +11,6 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
-variable public_key_path1 {
-  description = "Path to the public key used for ssh access"
-}
-
-variable public_key_path2 {
-  description = "Path to the public key used for ssh access"
-}
-
 variable private_key_path {
   description = "Path to the private key used by provisioners"
 }
