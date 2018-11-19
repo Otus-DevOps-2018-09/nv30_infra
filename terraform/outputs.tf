c007d4143c857f88677a5d5d9f4c2ed85402f88c
Nikita Gorlov <nv30over@gmail.com>
2018-11-19 12:07:06 -0500

    Removed obsolete files

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "app_external_ip" {
-  value = "${module.app.app_external_ip}"
-}
-
-#output "app-lb_external_ip" {
-#  value = "${google_compute_global_forwarding_rule.forwarding.ip_address}"
-#}
-
